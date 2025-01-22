a6959d25bfa9a8780b03009b2aa42f60f0baf88c
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-01-22 15:50:54 +0100

    add: arrays

---
+++ snack-03/main.js
@@ -0,0 +1,6 @@
+// Scrivere una funzione che accetti tre argomenti, un array ( esempio: ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'] )e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con li elementi estratti dall'array di partenza che hanno la posizione compresa tra i due numeri (a e b).
+
+const nomi = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+const a = 1;
+const b = 4;
