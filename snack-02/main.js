dfebdf1dd132f777159be355ab2d541ac31a1a56
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-01-22 15:18:43 +0100

    add: array squadreDiCalcio

--- snack-02/main.js
+++ snack-02/main.js
@@ -4,3 +4,30 @@
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
+const squadreDiCalcio = [
+    {
+        nome: "Inter",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Milan",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Juventus",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Roma",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Napoli",
+        puntiFatti: 0,
+        falliSubiti: 0
+    }
+];
