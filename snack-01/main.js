4a0f5a17e4bff2cc10ef8267de03f36b0329bfcd
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-01-22 15:02:49 +0100

    add: array biciDaCorsa

--- snack-01/main.js
+++ snack-01/main.js
@@ -1,3 +1,26 @@
 // Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
+
+const biciDaCorsa = [
+    {
+        nome: "Bici da corsa 1",
+        peso: 7.5 // peso in kg
+    },
+    {
+        nome: "Bici da corsa 2",
+        peso: 6.8 // peso in kg
+    },
+    {
+        nome: "Bici da corsa 3",
+        peso: 7.3 // peso in kg
+    },
+    {
+        nome: "Bici da corsa 4",
+        peso: 7.1 // peso in kg
+    },
+    {
+        nome: "Bici da corsa 5",
+        peso: 7.4 // peso in kg
+    }
+];
